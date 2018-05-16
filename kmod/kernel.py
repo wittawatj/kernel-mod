e5e8694857ba4a2118a5eeb241eca57cb74767be
Heishiro Kanagawa <heishiro.kanagawa@gmail.com>
2018-05-16 20:39:31 +0100

    add a KID kernel

--- kmod/kernel.py
+++ kmod/kernel.py
@@ -25,3 +25,19 @@ class KHoPoly(Kernel):
 
 # end of KHoPoly
 
+
+class KKID(Kernel):
+    """KID Kernel"""
+
+    def eval(self, X1, X2):
+        d = X1.shape[1]
+        assert d == X2.shape[1]
+        return (np.dot(X1, X2.T)/d + 1.)**3
+
+    def pair_eval(self, X, Y):
+        d = X1.shape[1]
+        assert d == Y.shape[1]
+        return (np.sum(X1, X2, axis=1)/d + 1.)**3
+
+    def __str__(self):
+        return 'KKID'
